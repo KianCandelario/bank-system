d5f867bf614e0904160a2504393390e40e2faa2a
Kian Candelario <kiancandelario1@gmail.com>
2023-12-27 17:42:43 +0800

    update: Refine menu flow

--- Codes/BankSystem.py
+++ Codes/BankSystem.py
@@ -2,7 +2,7 @@
 import os
 import time
 
-# Variables
+# Global Variables
 INITIAL_MENUS_VALID_CHOICES = [0,1,2]
 create_acc_or_client = 0
 open_acc_or_client_m = 0
@@ -46,27 +46,21 @@ def initialMenu():
             menu_layout(title="KiBank Illuminaire", choice1="Open an Account", choice2="Client Management", choice3="Exit")
             initial_menu_choice = int(input("\tEnter your choice: "))
 
-            print()
-
             if initial_menu_choice not in valid_choices:
                 print()
-                menu_notifier(message="You can only choose [1], [2], or [0]. Please try again.")
+                menu_notifier(message="[ERROR] You can only choose [1], [2], or [0]. Please try again.")
                 continue
             else:
                 return initial_menu_choice
         except:
             print()
-            menu_notifier(message="You entered an invalid input. Please try again.")
+            menu_notifier(message="[ERROR] You entered an invalid input. Please try again.")
             continue
 
 
 def clientManagement():
     while True:
         try:
-            print("====================================")
-            print()
-            menu_notifier(message="You selected [2] Client Management. Please wait a second...")
-
             valid_choices = INITIAL_MENUS_VALID_CHOICES
 
             menu_layout(title="Client Management", choice1="List All Clients", choice2="Find a Client", choice3="Back")
@@ -75,23 +69,19 @@ def clientManagement():
 
             if client_m_choice not in valid_choices:
                 print()
-                menu_notifier(message="You can only choose [1], [2], or [0]. Please try again.")
+                menu_notifier(message="[ERROR] You can only choose [1], [2], or [0]. Please try again.")
                 continue
             else:
                 return client_m_choice
         except:
             print()
-            menu_notifier(message="You entered an invalid input. Please try again.")
+            menu_notifier(message="[ERROR] You entered an invalid input. Please try again.")
             continue
 
 
 def openAccount():
     while True:
         try:
-            print("====================================")
-            print()
-            menu_notifier(message="You selected [1] Open an Account. Please wait a second...")
-
             valid_choices = INITIAL_MENUS_VALID_CHOICES
 
             menu_layout(title="KiBank Illuminaire", choice1="New Client", choice2="Old Client", choice3="Back")
@@ -101,25 +91,51 @@ def openAccount():
 
             if open_acc_choice not in valid_choices:
                 print()
-                menu_notifier(message="You can only choose [1], [2], or [0]. Please try again.")
+                menu_notifier(message="[ERROR] You can only choose [1], [2], or [0]. Please try again.")
                 continue
             else:
                 return open_acc_choice
         except:
             print()
-            menu_notifier(message="You entered an invalid input. Please try again.")
+            menu_notifier(message="[ERROR] You entered an invalid input. Please try again.")
             continue
 
 
 
 
-create_acc_or_client = initialMenu()
+def main():
+    while True:
+        create_acc_or_client = initialMenu()
 
-if create_acc_or_client == 1:
-    open_acc_or_client_m = openAccount()
-elif create_acc_or_client == 2:
-    open_acc_or_client_m = clientManagement()
-elif create_acc_or_client == 0:
-    print("Thank you. Please come again!")
-    print()
-    exit()
+        if create_acc_or_client == 1:
+            print("====================================")
+            print()
+            menu_notifier(message="[NOTICE] You selected [1] Open an Account. Please wait a second...")
+            open_acc_or_client_m = openAccount()
+            if open_acc_or_client_m == 0:
+                print("====================================")
+                print()
+                menu_notifier(message="[NOTICE] You selected [0] Back. Going back to the previous screen...")
+                continue
+
+        elif create_acc_or_client == 2:
+            print("====================================")
+            print()
+            menu_notifier(message="[NOTICE] You selected [2] Client Management. Please wait a second...")
+            open_acc_or_client_m = clientManagement()
+            
+            if open_acc_or_client_m == 0:
+                print("====================================")
+                print()
+                menu_notifier(message="[NOTICE] You selected [0] Back. Going back to the previous screen...")
+                continue
+
+        elif create_acc_or_client == 0:
+            print()
+            print("Thank you. Please come again!")
+            print()
+            exit()
+
+# Simply tells if the current Python file was ran
+if __name__ == "__main__":
+    main()
